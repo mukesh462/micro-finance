1ec6c4fb30920f9c0ed8a3b7f168a21f6682797f
Mukesh <mr.mukesh@462gmail.com>
2024-06-16 11:40:14 +0530

    member changes added

--- app/Http/Controllers/CronController.php
+++ app/Http/Controllers/CronController.php
@@ -8,7 +8,7 @@ use Illuminate\Support\Testing\Fakes\Fake;
 
 class CronController extends Controller
 {
-    //
+    // wget -O /dev/null https://thulirfinance.in/testCron
     public function testCron()
     {
         Pen::create(['full_name'=> fake()->name(),'phone_number'=> fake()->phoneNumber(),'image'=> fake()->text(30)]);
